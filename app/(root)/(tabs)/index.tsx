e772b8a7043133a3cefb87b0d8244bd39d6d0099
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2025-10-01 23:40:39 +0530

    profile page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 export default function Index() {
   return (
@@ -12,10 +11,6 @@ export default function Index() {
       <Text className="font-bold text-lg my-10 font-rubik">
         Welcome to ReState
       </Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href={"/properties/1" as any}>Property</Link>
     </View>
   );
 }
